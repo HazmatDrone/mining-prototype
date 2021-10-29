91f8989db587072cb03d0b048d945e3972b022f1
VeryBlank <Very_Blank@hotmail.com>
2021-10-29 01:03:44 -0400

    movement and spacebar damage

--- Playermodels/player.gd
+++ Playermodels/player.gd
@@ -3,7 +3,9 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export var speed = 100
+export var move_speed = 500
+export var max_health = 100
+var health = max_health
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,4 +14,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	
+	var speed = Vector2(0,0)
+	
+	if health > 0:
+		if Input.is_action_pressed("move_up"):
+			speed.y -= move_speed
+		if Input.is_action_pressed("move_down"):
+			speed.y += move_speed
+		if Input.is_action_pressed("move_right"):
+			speed.x += move_speed
+		if Input.is_action_pressed("move_left"):
+			speed.x -= move_speed
+
+		move_and_slide(100 * speed * delta)
+		
+	if Input.is_action_just_pressed("ui_select"):
+		health -= 50
+	if health == 0:
+		rotation_degrees = 180
